c99c4377490cf8047fe6a956b1db85c10b58bc71
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2023-02-24 02:11:07 +0530

    Add: package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Webcam from 'react-webcam';
+import { useRef } from 'react';
 function App() {
+  const webcamRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Dev-Delight
+      <Webcam
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              // onUserMediaError={showError}
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                textAlign: "center",
+                borderRadius: "20px",
+                zindex: 9,
+                width: 640,
+                height: 480,
+                display: "block",
+              }}
+            />
     </div>
   );
 }
